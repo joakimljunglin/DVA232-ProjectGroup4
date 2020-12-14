301145779fa561d6048696fbef9d6e2a6ce6e886
den027 <73971813+den027@users.noreply.github.com>
2020-12-14 14:45:20 +0100

    Remove bottom sheet

--- app/src/main/java/com/example/projectdva232v1/ui/learning_activities/WritingActivity.kt
+++ app/src/main/java/com/example/projectdva232v1/ui/learning_activities/WritingActivity.kt
@@ -2,14 +2,20 @@ package com.example.projectdva232v1.ui.learning_activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.example.projectdva232v1.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class WritingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_writing)
 
-        //TODO: Set margin/padding or something on the text, also make the font bigger (look at conventions)
+        initView()
+    }
+
+    private fun initView() {
     }
 }
