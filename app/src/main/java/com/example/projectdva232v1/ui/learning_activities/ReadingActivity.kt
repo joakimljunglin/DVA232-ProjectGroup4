31f18e9ebf1c786c2f71d141d3a270a53cf29c35
den027 <73971813+den027@users.noreply.github.com>
2020-12-15 00:13:54 +0100

    Add question bar

--- app/src/main/java/com/example/projectdva232v1/ui/learning_activities/ReadingActivity.kt
+++ app/src/main/java/com/example/projectdva232v1/ui/learning_activities/ReadingActivity.kt
@@ -2,11 +2,45 @@ package com.example.projectdva232v1.ui.learning_activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
 import com.example.projectdva232v1.R
+import com.google.android.material.chip.ChipGroup
 
 class ReadingActivity : AppCompatActivity() {
+    lateinit var btn: Button
+    lateinit var chips: ChipGroup
+    lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reading)
+
+        initView()
+    }
+
+    private fun initView() {
+        btn = findViewById(R.id.button)
+        chips = findViewById(R.id.chips)
+        progressBar = findViewById(R.id.progressBar)
+
+        // Next button should not be enabled until an answer has been selected
+        btn.isEnabled = false
+        btn.setOnClickListener {
+            // Clear all selections
+            chips.clearCheck()
+
+            // TODO: Remove below code
+            progressBar.progress++
+        }
+
+        // For when a chip is clicked
+        chips.setOnCheckedChangeListener { _, _ ->
+            // Enable continue button
+
+            // Make sure the button does not get enabled when a chip is deselected
+            btn.isEnabled = chips.checkedChipId != View.NO_ID
+        }
     }
 }
