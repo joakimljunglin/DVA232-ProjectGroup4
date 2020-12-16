5b77aeb89b546a193559604611853064df0d9496
den027 <73971813+den027@users.noreply.github.com>
2020-12-16 15:15:49 +0100

    Set progressbar

--- app/src/main/java/com/example/projectdva232v1/ui/learning_activities/ReadingActivity.kt
+++ app/src/main/java/com/example/projectdva232v1/ui/learning_activities/ReadingActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
 import com.example.projectdva232v1.R
+import com.example.projectdva232v1.ui.learning_activities.classes.Choice
 import com.example.projectdva232v1.ui.learning_activities.classes.Question
 import com.example.projectdva232v1.ui.learning_activities.classes.ReadingQuiz
 import com.example.projectdva232v1.ui.learning_activities.utilities.getJsonDataFromAsset
@@ -18,7 +19,8 @@ class ReadingActivity : AppCompatActivity() {
     lateinit var btn: Button
     lateinit var chips: ChipGroup
     lateinit var progressBar: ProgressBar
-    lateinit var questions: List<Question>
+    lateinit var questions: MutableList<Question>
+    var currentQuestion = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,17 +41,18 @@ class ReadingActivity : AppCompatActivity() {
             val mapper = jacksonObjectMapper()
             var quiz: ReadingQuiz = mapper.readValue<ReadingQuiz>(jsonFileString)
 
-            // TODO: Add questions to questions list
+            // Add questions to questions list
+            questions = mutableListOf<Question>()
+            var i = 1
             for (item in quiz.items) {
-                Log.d("DEBUG", item.text1)
-                Log.d("DEBUG", item.text2)
+                val mutableList = mutableListOf<Choice>()
                 for (choice in item.choices) {
-                    Log.d("DEBUG", choice.text)
-                    Log.d("DEBUG", choice.correct.toString())
+                    mutableList.add(Choice(choice.text, choice.correct))
                 }
+                questions.add(Question("Choose answer $i", mutableList))
+                i++
             }
 
-            // Populate questions
         } else {
             // TODO
             // Could not read the quiz json
@@ -62,14 +65,20 @@ class ReadingActivity : AppCompatActivity() {
         chips = findViewById(R.id.chips)
         progressBar = findViewById(R.id.progressBar)
 
+        // Set progressbar max to number of total questions
+        progressBar.max = questions.size
+
+        loadQuestion(currentQuestion)
+
         // Next button should not be enabled until an answer has been selected
         btn.isEnabled = false
         btn.setOnClickListener {
             // Clear all selections
             chips.clearCheck()
 
-            // TODO: Remove below code
-            progressBar.progress++
+            // Continue to next question
+            progressBar.progress = currentQuestion
+            currentQuestion++
         }
 
         // For when a chip is clicked
@@ -79,5 +88,12 @@ class ReadingActivity : AppCompatActivity() {
             // Make sure the button does not get enabled when a chip is deselected
             btn.isEnabled = chips.checkedChipId != View.NO_ID
         }
+    }
+
+    private fun loadQuestion(currentQuestion: Int) {
+        // Changes the chips and text to respond to the given question number
+
+        // Consider that the number of answers may not always be 4
+        // Therefore the number of chips needs to be dynamic
     }
 }
