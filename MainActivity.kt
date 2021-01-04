db682de6af4c13dbe6b43142fdbeae5d9d8199d5
ediuva <74615322+ediuva@users.noreply.github.com>
2021-01-04 11:21:00 +0100

    Select Activity suggestions applied
    files modified to apply the suggestions made
    (keep difficulty chosen highlighted, avoid adding activity on back pressed, inversion of two activities)

---
+++ MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.projectdva232v1
+
+import android.R.attr.button
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var drawerLayout:DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+       drawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+                setOf(
+                        R.id.nav_home,
+                        R.id.nav_listening,
+                        R.id.nav_vocabulary,
+                        R.id.nav_writing,
+                        R.id.nav_speaking,
+                        R.id.nav_reading
+                ), drawerLayout
+        )
+
+        NavigationUI.setupWithNavController(navView, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ RecyclerAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.projectdva232v1.ui.homePage
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projectdva232v1.R
+
+var lastClickedPosition = -1
+class RecyclerAdapter(
+        private val difficultyList: List<DifficultyLevelItem>,
+        private val listener: RecyclerAdapterActivity.OnItemClickListener
+) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+    // Called for every ViewHolder item that is to be created
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // Build view from XML file
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_difficultyitem,
+                parent, false)
+
+        return ViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return difficultyList.size
+    }
+
+    // Called each time a new ViewHolder is to be displayed
+    // Takes ViewHolder and fills it with the new data to be displayed
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = difficultyList[position]
+        holder.textDifficulty.text = currentItem.difficultyName
+
+        holder.diffLayout.setOnClickListener {
+            lastClickedPosition = position
+            notifyDataSetChanged()
+            if (position != RecyclerView.NO_POSITION) {
+                listener.onItemClicked(holder.textDifficulty.text.toString())
+            }
+        }
+        if (lastClickedPosition == position) {
+            holder.textDifficulty.setBackgroundResource(R.color.pink)
+        }
+    }
+
+    // Class with the data that is displayed for each Difficulty Item
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val textDifficulty: TextView = itemView.findViewById(R.id.difficulty_tv)
+        val diffLayout: RelativeLayout = itemView.findViewById(R.id.diff_layout)
+    }
+}
