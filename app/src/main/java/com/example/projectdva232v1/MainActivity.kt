5840c33d63f9b0aa4342c024d48d8d535e0dab04
ediuva <74615322+ediuva@users.noreply.github.com>
2021-01-11 13:51:25 +0100

    Removed navigation drawer

--- app/src/main/java/com/example/projectdva232v1/MainActivity.kt
+++ app/src/main/java/com/example/projectdva232v1/MainActivity.kt
@@ -17,48 +17,11 @@ import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var drawerLayout: DrawerLayout
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-setContentView(R.layout.activity_main)
-val toolbar: Toolbar = findViewById(R.id.toolbar)
-setSupportActionBar(toolbar)
-
-drawerLayout = findViewById(R.id.drawer_layout)
-val navView: NavigationView = findViewById(R.id.nav_view)
-val navController = findNavController(R.id.nav_host_fragment)
-// Passing each menu ID as a set of Ids because each
-// menu should be considered as top level destinations.
-appBarConfiguration = AppBarConfiguration(
-        setOf(
-                R.id.nav_home,
-                R.id.nav_vocabulary,
-                R.id.nav_listening,
-                R.id.nav_writing,
-                R.id.nav_speaking,
-                R.id.nav_reading
-        ), drawerLayout
-)
-
-NavigationUI.setupWithNavController(navView, navController)
-NavigationUI.setupActionBarWithNavController(this, navController)
-
 
         val intent = Intent(this, HomePageClass::class.java)
         startActivity(intent)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-// Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -70,6 +33,4 @@ NavigationUI.setupActionBarWithNavController(this, navController)
             else -> super.onOptionsItemSelected(item)
         }
     }
-
-
 }
